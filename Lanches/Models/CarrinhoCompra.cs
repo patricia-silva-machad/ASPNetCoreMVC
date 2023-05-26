d3a38b004daa4e2809fbf259e6f3d4dd91c20883
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-05-26 14:15:14 -0300

    Adicionando itens ao carrinho

--- Lanches/Models/CarrinhoCompra.cs
+++ Lanches/Models/CarrinhoCompra.cs
@@ -16,22 +16,38 @@ namespace Lanches.Models
             //define uma sessão
             ISession session =
                 services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
-
             //obtem um serviço do tipo do nosso contexto
             var context = services.GetService<AppDbContext>();
-
             //obtem ou gera o ID do carrinho da session
             string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
-
             //se for nulo ?? atribui o id do carrinho na sessão
             session.SetString("CarrinhoId", carrinhoId);
-
             //retorna o carrinho com o contexto e o Id atribuido ou obtido
             return new CarrinhoCompra(context)
             {
                 CarrinhoCompraId = carrinhoId
             };
         }
-
+        public void AdicionarAoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                s => s.Lanche.LancheId == lanche.LancheId && 
+                s.CarrinhoCompraId == CarrinhoCompraId);
+            if(carrinhoCompraItem == null) 
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            } 
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+        }
     }
 }
