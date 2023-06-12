c83ac105bd5ced2a47e2dfc9ca7eb6323df0d74e
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-06-12 17:56:58 -0300

    implementando logout na aplicação

--- Lanches/Controllers/AccountController.cs
+++ Lanches/Controllers/AccountController.cs
@@ -78,5 +78,14 @@ namespace Lanches.Controllers {
             }
             return View(registroVM);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout() 
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
