a484e7c7d68559bda14ce107a856d9938579def5
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-06-12 22:36:43 -0300

    Conceitos de area. Adicionando area admin na aplicação

---
+++ Lanches/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Areas.Admin.Controllers {
+    [Area("Admin")]
+    public class AdminController : Controller {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
