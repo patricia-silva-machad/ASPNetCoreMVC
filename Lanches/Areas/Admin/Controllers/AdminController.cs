02b58bb5afcda5cee4d7ebc226a9c59be93cc3f0
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-06-15 20:02:07 -0300

    correcao da branch

--- Lanches/Areas/Admin/Controllers/AdminController.cs
+++ Lanches/Areas/Admin/Controllers/AdminController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Lanches.Areas.Admin.Controllers {
     [Area("Admin")]
-    [Authorize("Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller 
     {
         public IActionResult Index() 
